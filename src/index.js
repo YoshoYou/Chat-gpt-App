436b7cbb53bd1b4006ec78516febe95bcfbc45e2
aya-jouney <www.ayajouney2@gmail.com>
2024-05-26 21:21:32 +0300

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-
-import App from './App';
-
- ReactDom.render(<App></App>, document.getElementById( 'root' ) ); //the app compnent will be render inside the div whit ip of root 
